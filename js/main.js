77a9a11eedbf5051dfb5ea5d1f267fa2d6a32091
Marta Guza <81906950+martunia880@users.noreply.github.com>
2023-03-11 13:39:33 +0100

    added js and slides starts working

--- js/main.js
+++ js/main.js
@@ -6,16 +6,30 @@ const formBox3 = document.getElementById("register");
 const formBox4 = document.getElementById("loginswitch");
 
 registerBtn.addEventListener("click", () => {
-    formBox4.classList.add("up");
-    formBox4.classList.add("show");
-    formBox3.classList.add("down");
+    formBox1.classList.add("down");
+    formBox1.classList.add("unshow");
+    formBox1.classList.remove("show");
+    formBox2.classList.add("up");
+    formBox2.classList.add("unshow");
+    formBox2.classList.remove("show");
+    formBox3.classList.add("up");
     formBox3.classList.add("show");
-
-    //test
-    formBox3.style.zIndex="10";
-
-
+    formBox3.classList.remove("unshow");
+    formBox4.classList.add("down");
+    formBox4.classList.add("show");
+    formBox4.classList.remove("unshow");
 })
 loginBtn.addEventListener("click", () => {
-    
+    formBox1.classList.remove("down");
+    formBox1.classList.remove("unshow");
+    formBox1.classList.add("show");
+    formBox2.classList.remove("up");
+    formBox2.classList.remove("unshow");
+    formBox2.classList.add("show");
+    formBox3.classList.remove("up");
+    formBox3.classList.remove("show");
+    formBox3.classList.add("unshow");
+    formBox4.classList.remove("down");
+    formBox4.classList.remove("show");
+    formBox4.classList.add("unshow");
 })
